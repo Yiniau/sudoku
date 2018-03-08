a82f344a3d31c92ffd2292badd331b98ec6bef99
yiniau <yiranzhouyida@gmail.com>
2018-03-08 12:37:53 +0800

    update Travis CI settings

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,11 +18,12 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin sudoku --target $TARGET --release -- -C lto
+    # cross rustc --bin sudoku --target $TARGET --release -- -C lto
+    cargo build --targets=$TARGET --release
 
     # TODO Update this to package the right artifacts
     cp target/$TARGET/release/sudoku $stage/
-    cp assets/FiraSans-Regular.ttf $stage/assets
+    cp assets/FiraSans-Regular.ttf $stage/assets/
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
